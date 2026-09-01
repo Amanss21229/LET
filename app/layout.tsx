15e19c5d007bcfa7000b475e2c40b5573a8de3bb
Amanss21229 <167667148+Amanss21229@users.noreply.github.com>
2026-09-02 01:35:18 +0530

    Refactor layout component to include AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,47 @@
 import "./globals.css";
-import type { Metadata } from "next";
+
+import type {
+  Metadata,
+} from "next";
+
+import {
+  AuthProvider,
+} from "@/components/AuthProvider";
+
 
 export const metadata: Metadata = {
-  title: "LET - Learn Earn Teach",
-  description: "Learn • Earn • Teach",
 
-  verification: {
-    google: "a8clSvgtk7cHYp7cRjpc54UPZKuBdOXmUcsgTLrKtJc",
-  },
+  title:
+    "LET - Learn Earn Teach",
+
+  description:
+    "Learn • Earn • Teach",
+
 };
+
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+
+    <html lang="en">
+
+      <body>
+
+        <AuthProvider>
+
+          {children}
+
+        </AuthProvider>
+
+      </body>
+
     </html>
+
   );
+
 }
