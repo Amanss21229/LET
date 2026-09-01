00740e35bcf68eb1acc3459db572a144683e21f0
Amanss21229 <167667148+Amanss21229@users.noreply.github.com>
2026-09-01 10:40:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,23 @@
-import "./globals.css"; import type { Metadata } from "next";
-export const metadata:Metadata={title:"LET - Learn Earn Teach",description:"Learn • Earn • Teach"};
-export default function Layout({children}:{children:React.ReactNode}){return <html lang="en"><body>{children}</body></html>}
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LET - Learn Earn Teach",
+  description: "Learn • Earn • Teach",
+
+  verification: {
+    google: "a8clSvgtk7cHYp7cRjpc54UPZKuBdOXmUcsgTLrKtJc",
+  },
+};
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
