7a2723f65b8aaadfa58e5c30dccd6b431dd0dadf
Amanss21229 <167667148+Amanss21229@users.noreply.github.com>
2026-09-02 08:47:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,19 @@
 import "./globals.css";
-
-import type {
-  Metadata,
-} from "next";
-
-import {
-  AuthProvider,
-} from "@/components/AuthProvider";
-
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-
-  title:
-    "LET - Learn Earn Teach",
-
-  description:
-    "Learn • Earn • Teach",
-
+  title: "LET - Learn Earn Teach",
+  description: "Learn • Earn • Teach",
 };
-
 
 export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   return (
-
     <html lang="en">
-
-      <body>
-
-        <AuthProvider>
-
-          {children}
-
-        </AuthProvider>
-
-      </body>
-
+      <body>{children}</body>
     </html>
-
   );
-
 }
