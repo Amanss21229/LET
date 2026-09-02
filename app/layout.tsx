a5cdfafdee61a91d1ae44c1a29232b7c052c5957
Amanss21229 <167667148+Amanss21229@users.noreply.github.com>
2026-09-02 11:29:27 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,52 @@
 import "./globals.css";
-import type { Metadata } from "next";
+
+import type {
+  Metadata,
+} from "next";
+
+import {
+  FirebaseAuthProvider,
+} from "@/components/FirebaseAuthProvider";
+
 
 export const metadata: Metadata = {
-  title: "LET - Learn Earn Teach",
-  description: "Learn • Earn • Teach",
+
+  title:
+    "LET - Learn Earn Teach",
+
+  description:
+    "Learn • Earn • Teach",
+
 };
 
+
 export default function Layout({
+
   children,
+
 }: {
-  children: React.ReactNode;
+
+  children:
+    React.ReactNode;
+
 }) {
+
   return (
+
     <html lang="en">
-      <body>{children}</body>
+
+      <body>
+
+        <FirebaseAuthProvider>
+
+          {children}
+
+        </FirebaseAuthProvider>
+
+      </body>
+
     </html>
+
   );
+
 }
