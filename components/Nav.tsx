c907c96c143e991a2109a0cb3a6bd81457ab1782
Amanss21229 <167667148+Amanss21229@users.noreply.github.com>
2026-09-02 12:03:37 +0530

    Refactor Nav component to use NavClient

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,74 +1,8 @@
-import Link from "next/link";
+import NavClient from "./NavClient";
 
-import { auth } from "@/lib/auth";
 
-import ThemeToggle from "./ThemeToggle";
+export default function Nav() {
 
-export default async function Nav() {
-  const session =
-    await auth();
+  return <NavClient />;
 
-  return (
-    <nav className="nav wrap">
-
-      <Link
-        className="brand"
-        href="/"
-      >
-        <img
-          src="/let-logo.png"
-          alt="LET logo"
-        />
-
-        LET
-      </Link>
-
-      <div className="links">
-
-        <Link href="/">
-          All Batches
-        </Link>
-
-        <Link href="/my-batches">
-          My Batches
-        </Link>
-
-        <Link href="/profile">
-          My Profile
-        </Link>
-
-        <Link href="/admin">
-          Admin
-        </Link>
-
-      </div>
-
-      <div className="nav-actions">
-
-        <ThemeToggle />
-
-        {!session?.user ? (
-
-          <a
-            className="btn primary"
-            href="/api/auth/signin/google"
-          >
-            Continue with Google
-          </a>
-
-        ) : (
-
-      <Link
-        className="btn primary"
-        href="/logout"
-      >
-        Logout
-      </Link>
-
-        )}
-
-      </div>
-
-    </nav>
-  );
 }
