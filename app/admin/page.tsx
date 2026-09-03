5b81a514862c00527431a50a2c9631d235cf068e
Amanss21229 <167667148+Amanss21229@users.noreply.github.com>
2026-09-03 11:04:31 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1250,6 +1250,7 @@ const deleteBatch =
             {[
               "All Users",
               "Create Batch",
+              "Edit / Delete Batch",    
               "Manage Batch",
               "About Tutor",
               "Chats",
